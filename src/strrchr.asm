8533944b528c78e20adbe9ff43d4acf3b781ed82
Thomaltarix <thomas.boue@epitech.eu>
2024-02-09 15:44:25 +0100

    ♻️ Refactor strrchr using strchr code

--- src/strrchr.asm
+++ src/strrchr.asm
@@ -3,42 +3,28 @@ SECTION .text           ; Code section
 GLOBAL strrchr        ; export "strrchr"
 
 strrchr:
-        ENTER 0,0       ; starts the program
-        CALL strlen     ; Calls the strlen function to get the length of the string
-        MOV RCX, RAX    ; Moves the length to RCX
+        ENTER 0, 0       ; starts the program
 
-        .loop:          ; loop to find the last c character
-                CMP RCX, 0      ; Checks if the length is 0
-                JE .not_found        ; If it is 0, then it is the end of the string
-                CMP BYTE [RDI + RCX - 1], SIL  ; Compares the last character with the character
-                JE .found           ; If it is the same, then it is found
-                DEC RCX             ; Decrements the length
-                JMP .loop           ; Jumps to the loop
-
-        .not_found:
-                MOV RAX, 0      ; If the character is not found, return 0
-                LEAVE           ; Ends the program
-                RET
-
-        .found:
-                MOV RAX, RDI    ; If the character is found, return the pointer
-                ADD RAX, RCX    ; Moves the pointer to the last character found
-                DEC RAX     ; Moves the pointer to the last character found
-                LEAVE           ; Ends the program
-                RET
-
-strlen:              ; Loops through the string to return its len
-        ENTER 0, 0              ; Starts the program
-        MOV RCX, 0              ; Initializes the counter
-
-        .loop:                  ; Loop which checks if the character is null or not
-                CMP BYTE [RDI], 0       ; Checks if the character is null
-                JE .end                 ; True -> jumps to the end
+        .start_loop:
+                CMP BYTE [RDI + 1], 0       ; Checks if it's the end of the string
+                JE .loop                ; True -> jump to the loop
                 INC RDI                 ; Increments the pointer
-                INC RCX                 ; False -> increments the length = the result
-                JMP .loop               ; Jumps to the loop
+                JMP .start_loop         ; Jumps to itself
 
-        .end:                   ; End of the program
-                MOV RAX, RCX            ; Moves the length to RAX
+        .loop:              ; loop to find the c character
+                CMP BYTE [RDI], 0       ; Checks if it's the end of the string
+                JE .not_found           ; True -> jump to the end
+                CMP BYTE [RDI], SIL     ; Checks if the caracters of s is c
+                JE .end                 ; True -> jump to the end
+                DEC RDI                 ; Decrements the pointer
+                JMP .loop               ; Jumps to itself
+
+        .end:                ; End of the program
+                MOV RAX, RDI            ; Moves the pointer found to RAX
                 LEAVE                   ; Ends the program
-                RET                     ; Returns the length of the string
+                RET                     ; Returns
+
+        .not_found:          ; Not found
+                MOV RAX, 0              ; Moves 0 to RAX
+                LEAVE                   ; Ends the program
+                RET                     ; Returns
