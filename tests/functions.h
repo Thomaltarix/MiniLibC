6ed291ec18e1ec53e3d73669a4c73fe9cb96c86e
Thomaltarix <thomas.boue@epitech.eu>
2024-02-09 16:30:21 +0100

    🧱 Update the header file with the memset function

--- tests/functions.h
+++ tests/functions.h
@@ -10,5 +10,6 @@
 int my_strlen(char *str);
 char *my_strchr(const char *s, int c);
 char *my_strrchr(const char *s, int c);
+void *my_memset(void *s, int c, size_t n);
 
 void redirect_all_std(void);
