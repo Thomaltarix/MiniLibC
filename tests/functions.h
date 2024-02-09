e57b649068178b6f01fb3ae733a71e886fc431cc
Thomaltarix <thomas.boue@epitech.eu>
2024-02-09 17:30:50 +0100

    🧱 Update the header file with the memcpy function

--- tests/functions.h
+++ tests/functions.h
@@ -11,5 +11,6 @@ int my_strlen(char *str);
 char *my_strchr(const char *s, int c);
 char *my_strrchr(const char *s, int c);
 void *my_memset(void *s, int c, size_t n);
+void *my_memcpy(void *restrict dest, const void *restrict src, size_t n);
 
 void redirect_all_std(void);
