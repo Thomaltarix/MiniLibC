8e2ba34bf1fbde64a27b2848159e8b81694126ed
Thomaltarix <thomas.boue@epitech.eu>
2024-02-19 21:27:38 +0100

    ✅ Add passing tests for the strpbrk

--- tests/tests_strpbrk.c
+++ tests/tests_strpbrk.c
@@ -12,6 +12,7 @@ char *my_strpbrk(const char *s, const char *accept)
     char *handle;
     char *(*my_strpbrk)(const char *, const char *);
     char *error;
+    char *result;
 
     handle = dlopen("./libasm.so", RTLD_LAZY);
     if (!handle) {
@@ -23,16 +24,112 @@ char *my_strpbrk(const char *s, const char *accept)
         fprintf(stderr, "%s\n", error);
         return NULL;
     }
-    my_strpbrk(s, accept);
+    result = my_strpbrk(s, accept);
     dlclose(handle);
-    return my_strpbrk(s, accept);
+    return result;
 }
 
 Test(strpbrk, simple_sentence)
 {
-    char *src = "Hello";
-    char *tmp;
+    char *test = "Hello, World!";
+    char *my_result;
+    char *result;
 
-    tmp = my_strpbrk(src, "l");
-    cr_assert_str_eq(tmp, "llo");
+    my_result = my_strpbrk(test, "World");
+    result = strpbrk(test, "World");
+    cr_assert_eq(my_result, result);
+}
+
+Test(strpbrk, long_sentence)
+{
+    char *test = "lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi\
+    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit\
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur\
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt\
+    mollit anim id est laborum.";
+    char *my_result;
+    char *result;
+
+    my_result = my_strpbrk(test, "ipsum");
+    result = strpbrk(test, "ipsum");
+    cr_assert_eq(my_result, result);
+}
+
+Test(strpbrk, not_found)
+{
+    char *test = "Hello, World!";
+    char *my_result;
+    char *result;
+
+    my_result = my_strpbrk(test, "zwv.yx");
+    result = strpbrk(test, "zwv.yx");
+    cr_assert_eq(my_result, result);
+}
+
+Test(strpbrk, empty_string)
+{
+    char *test = "";
+    char *my_result;
+    char *result;
+
+    my_result = my_strpbrk(test, "World");
+    result = strpbrk(test, "World");
+    cr_assert_eq(my_result, result);
+}
+
+Test(strpbrk, empty_accept)
+{
+    char *test = "Hello, World!";
+    char *my_result;
+    char *result;
+
+    my_result = my_strpbrk(test, "");
+    result = strpbrk(test, "");
+    cr_assert_eq(my_result, result);
+}
+
+Test(strpbrk, empty_string_and_accept)
+{
+    char *test = "";
+    char *my_result;
+    char *result;
+
+    my_result = my_strpbrk(test, "");
+    result = strpbrk(test, "");
+    cr_assert_eq(my_result, result);
+}
+
+Test(strpbrk, zero_ended_string)
+{
+    char *test = "Hello, World!\0";
+    char *my_result;
+    char *result;
+
+    my_result = my_strpbrk(test, "World");
+    result = strpbrk(test, "World");
+    cr_assert_eq(my_result, result);
+}
+
+Test(strpbrk, zero_ended_string_only)
+{
+    char *test = "";
+    char *my_result;
+    char *result;
+
+    my_result = my_strpbrk(test, "\0");
+    result = strpbrk(test, "\0");
+    cr_assert_eq(my_result, result);
+}
+
+Test(strpbrk, zero_ended_string_accept)
+{
+    char *test = "Hello, World!";
+    char *my_result;
+    char *result;
+
+    my_result = my_strpbrk(test, "\0");
+    result = strpbrk(test, "\0");
+    cr_assert_eq(my_result, result);
 }
